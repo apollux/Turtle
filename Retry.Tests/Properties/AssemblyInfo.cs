d96923cab112f93dc581055f9fb598f48150aebc
denxorz <denxorz@gmail.com>
2016-05-20 22:58:09 +0200

    Removed COM GUIDs from AssemblyInfo

--- Retry.Tests/Properties/AssemblyInfo.cs
+++ Retry.Tests/Properties/AssemblyInfo.cs
@@ -1,11 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Turtle.Retry.Tests")]
 [assembly: AssemblyDescription("")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ea8733d-4a30-44c2-b71e-1b1617167ea1")]
