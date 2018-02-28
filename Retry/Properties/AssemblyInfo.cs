1c6920904811513cb2983f6631939e8f118d56f7
Andre Kwakernaak <a.kwakernaak@flexposure.nl>
2018-02-28 17:52:38 +0100

    Target .netstandard

--- Retry/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Turtle.Retry")]
-[assembly: AssemblyDescription("Helps to get rid of repetitive retry logic.")]
-
-[assembly: InternalsVisibleTo("Turtle.Retry.Tests")]
